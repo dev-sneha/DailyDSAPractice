85c934d754a10e5c1264e7c85d008bfd65da03ad
snehasharma <parasharisneha@gmail.com>
2023-08-19 16:16:42 +0530

    half pyramid

---
+++ src/patterns/HalfPyramid.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("9");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+      /* output
+        9
+        99
+        999
+        9999  */
+
+
+
