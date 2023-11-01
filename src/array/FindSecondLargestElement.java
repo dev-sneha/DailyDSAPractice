302d4f9fbc673d4f9383b9d75e818bf60be8e2ac
snehasharma <parasharisneha@gmail.com>
2023-11-01 13:34:43 +0530

    Add existing project files to Git

--- src/array/FindSecondLargestElement.java
+++ src/array/FindSecondLargestElement.java
@@ -1,2 +1,39 @@
-package array;public class FindSecondLargestElement {
+package array;
+
+//Second-Largest Element in the Array
+public class FindSecondLargestElement {
+
+    public static void main(String[] args) {
+
+        int arr[] ={2,4,5,6,8,10,11};
+        int max=Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                secondMax=max;
+                max=arr[i];
+
+            } else if (arr[i]>secondMax && arr[i]!=max) {
+
+                secondMax=arr[i];
+
+
+            }
+
+        }
+
+
+        System.out.println(secondMax);
+
+
+
+
+
+
+
+
+
+    }
+
 }
